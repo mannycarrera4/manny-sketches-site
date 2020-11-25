6177fe5fbebc0c57114d2726e5dcca70cfbd262c
manuel carrera <manuel.carrera@workday.com>
2020-11-25 16:23:51 -0700

    Updates

--- component---src-pages-page-2-js-0babd8072ea9efba65fb.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{p5nM:function(e,t,n){"use strict";n.r(t);n("q1tI");var o=n("Wbzz"),b=n("9Dj+"),c=n("vrFN"),a=n("qKvR");t.default=function(){return Object(a.b)(b.a,null,Object(a.b)(c.a,{title:"Page two"}),Object(a.b)("h1",null,"Hi from the second page"),Object(a.b)("p",null,"Welcome to page 2"),Object(a.b)(o.Link,{to:"/"},"Go back to the homepage"))}}}]);
-//# sourceMappingURL=component---src-pages-page-2-js-0babd8072ea9efba65fb.js.map
