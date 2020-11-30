d62aaf45012cdab335447539deb1cd9af524bf31
manuel carrera <manuel.carrera@workday.com>
2020-11-29 17:21:00 -0700

    chore: Start adding to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,19 @@
 import React from "react"
-const AboutPage = () => (
-  <main>
-    <h1>About the Author</h1>
-    <p>Welcome to my Gatsby site.</p>
-  </main>
-)
+import Layout from "../components/layout"
+import { Flex, Center, Heading } from "@chakra-ui/react"
+
+const AboutPage = () => {
+  return (
+    <Layout>
+      <Flex align="center" justify="center">
+        <Center w="700px">
+          <Heading size="2xl" textAlign="center">
+            I suck at drawing, which is why I got into sketching.
+          </Heading>
+        </Center>
+      </Flex>
+    </Layout>
+  )
+}
+
 export default AboutPage
