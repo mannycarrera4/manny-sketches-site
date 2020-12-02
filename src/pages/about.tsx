144e5ecfc350d3c618ab2ee36898a5b196dfe465
manuel carrera <manuel.carrera@workday.com>
2020-12-01 22:57:03 -0700

    chore: Add more to about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,8 +1,7 @@
 import React from "react"
 import Layout from "../components/layout"
-import { Flex, Center, Heading } from "@chakra-ui/react"
+import { Flex, Center, Heading, Text } from "@chakra-ui/react"
 import ImageAbout from "../components/imageAbout"
-import { motion } from "framer-motion"
 
 const AboutPage = () => {
   return (
@@ -14,8 +13,31 @@ const AboutPage = () => {
             in love with the <span style={{ color: "#02c85e" }}>mountains</span>
           </Heading>
         </Center>
-        <div style={{ width: "80%" }}>
+        <div style={{ width: "80%", marginBottom: "30px" }}>
           <ImageAbout />
+        </div>
+        <div style={{ maxWidth: "750px" }}>
+          <Text fontSize="22px" mb="30px">
+            I was born in the Dominican Republic, but made my way to Colorado.
+            When I was younger, I got into drawing when one of my friends taught
+            me how to draw Goku from Dragon Ball Z.{" "}
+          </Text>
+          <Text fontSize="22px" mb="30px">
+            I had no idea what sketching was until I got to highschool and had
+            an amazing art teacher that showed me the freedom of sketching. My
+            medium of choice was originally pencil, but later explored how messy
+            and perfectly un perfect water colors were.
+          </Text>
+          <Text fontSize="22px" mb="30px">
+            Later in college, I had the chance to participate in an Urban
+            Sketchers Symposium. This was the first time I experienced such a
+            supportive and diverse community of artists and I fell in love with
+            it.
+          </Text>
+          <Text fontSize="22px" mb="30px">
+            The art you see on here are a mix of on site sketches, commissions,
+            experiences, and a little of what makes me happy. I hope you enjoy!
+          </Text>
         </div>
       </Flex>
     </Layout>
